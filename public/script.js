f293409362d034f890c771356015358410d75cdf
Mamirjon Mamadaliev <mike030188@gmail.com>
2021-06-27 19:04:57 +0900

    ability to allow others to stream their video

--- public/script.js
+++ public/script.js
@@ -5,6 +5,12 @@ const videoGrid = document.getElementById('video-grid');
 const myVideo = document.createElement('video');
 myVideo.muted = true;
 
+var peer = new Peer(undefined, {
+    path: '/peerjs',
+    host: '/',
+    port: '3030'
+});
+
 let myVideoStream
 navigator.mediaDevices.getUserMedia({
     video: true,
@@ -12,10 +18,34 @@ navigator.mediaDevices.getUserMedia({
 }).then(stream => {
     meVideoStream = stream;
     addVideoStream(myVideo, stream);
+
+    peer.on('call', call => {
+        call.answer(stream)
+        const video = document.createElement('video')
+        call.on('stream', userVideoStream => {
+            addVideoStream(video, userVideoStream)
+        })
+    })
+
+    socket.on('user-connected', (userId) => {
+        connectToNewUser(userId, stream);
+    })
+    
+})
+
+peer.on('open', id => {
+    socket.emit('join-room', ROOM_ID, id);
 })
 
 
-socket.emit('join-room');
+
+const connectToNewUser = (userId, stream) => {
+    const call = peer.call(userId, stream)
+    const video = document.createElement('video')
+    call.on('stream', userVideoStream => {
+        addVideoStream(video, userVideoStream)
+    })
+}
 
 const addVideoStream = (video, stream) => {
     video.srcObject = stream;
